6bcda584575f509ab2bb665e8c4a8bfa92df2ad4
miranda-ni <eralieva.miranda@gmail.com>
2020-02-10 00:02:28 +0600

    ggg

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -4,13 +4,8 @@ public class Person  {
     private int age;
     private String name;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public Person(int age, String name) {
         this.age = age;
